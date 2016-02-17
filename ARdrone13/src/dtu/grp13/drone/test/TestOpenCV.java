9269f681f807d2d052d20c83de098307d0f913d6
champen <champen@champen-Pc>
2016-02-17 15:43:50 +0100

    Test til at checke at api's virker er sat op

---
+++ ARdrone13/src/dtu/grp13/drone/test/TestOpenCV.java
@@ -0,0 +1,16 @@
+package dtu.grp13.drone.test;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class TestOpenCV {
+	
+	   public static void main( String[] args )
+	   {
+	      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+	      Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+	      System.out.println( "mat = \n" + mat.dump() );
+	   }
+
+}
