34ca20d3654c5127b4e3ccdcaf92a9c3474a95d7
Nicopus <Nicolaihansen@live.dk>
2016-06-09 12:49:53 +0200

    mere stuff

---
+++ ARdrone13/src/dtu/grp13/drone/core/matproc/TestProcess.java
@@ -0,0 +1,12 @@
+package dtu.grp13.drone.core.matproc;
+
+import org.opencv.core.Mat;
+
+public class TestProcess extends AbstractProcess {
+
+	@Override
+	public Mat processMat(Mat a) {
+		return a;
+	}
+
+}
