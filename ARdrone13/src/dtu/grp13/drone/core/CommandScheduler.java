86fbddeaaab5da210e6464f508126f42dcd50d3f
Sazo <Sazo@sazvans-mbp.eduroam.wireless.dtu.dk>
2016-06-17 12:24:33 +0200

    bud på scheduler

--- ARdrone13/src/dtu/grp13/drone/core/CommandScheduler.java
+++ ARdrone13/src/dtu/grp13/drone/core/CommandScheduler.java
@@ -1,6 +1,7 @@
 package dtu.grp13.drone.core;
 
 import de.yadrone.base.IARDrone;
+import de.yadrone.base.command.VideoChannel;
 
 public class CommandScheduler implements ICommandThread {
 
@@ -13,49 +14,86 @@ private IARDrone drone;
 
 	@Override
 	public void forward(int speed, int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().forward(speed);
+				drone.getCommandManager().hover().doFor(100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void left(int speed, int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().goLeft(speed);
+				drone.getCommandManager().hover().doFor(100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void right(int speed, int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().goRight(speed);
+				drone.getCommandManager().hover().doFor(100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void backward(int speed, int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().backward(speed);
+				drone.getCommandManager().hover().doFor(100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void takeOff() throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().takeOff();
+				drone.getCommandManager().flatTrim().doFor(1000);
+			}
+		});
 		
 	}
 
 	@Override
 	public void land() throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().landing();
+			}
+		});
 		
 	}
 
 	@Override
 	public void waitFor(int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().waitFor(milsec);
+			}
+		});
 		
 	}
 
 	@Override
 	public void hover(int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().hover().doFor(milsec);
+			}
+		});
 		
 	}
 
@@ -67,74 +105,97 @@ private IARDrone drone;
 
 	@Override
 	public void up(int speed, int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().up(speed).doFor(milsec);
+				drone.getCommandManager().hover().doFor(100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void down(int speed, int milsec) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().down(speed).doFor(milsec);
+				drone.getCommandManager().hover().doFor(100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void emegency() {
-		// TODO Auto-generated method stub
+		this.drone.getCommandManager().emergency();
 		
 	}
 
 	@Override
 	public void rotateClockwise(int angle) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().spinRight(15).doFor(angle*100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void rotateCounterClockwise(int angle) throws InterruptedException {
-		// TODO Auto-generated method stub
+		drone.getCommandManager().schedule(2000, new Runnable(){
+			public void run(){
+				drone.getCommandManager().spinLeft(15).doFor(angle*100);
+			}
+		});
 		
 	}
 
 	@Override
 	public void stepForward() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		this.forward(20, 500);
 	}
 
 	@Override
 	public void stepBackward() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		this.backward(20, 500);
 	}
 
 	@Override
 	public void stepLeft() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		this.left(20, 500);
 	}
 
 	@Override
 	public void stepRight() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		this.right(20, 500);
 	}
-
+		
 	@Override
 	public void next() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				drone.getCommandManager().setVideoChannel(VideoChannel.NEXT);
+			}});
+		t.start();
+		t.join();
 	}
 
 	@Override
 	public void stabilize() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				drone.getCommandManager().flatTrim();
+			}});
+		t.start();
+		t.join();
 	}
 
 	@Override
 	public void stop() throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		drone.stop();
 	}
 	
 }
